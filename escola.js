67acae1b61bfc84899912855c8d1a6ed49502b5f
rafaeldasilvamuniz <86721430+rafaeldasilvamuniz@users.noreply.github.com>
2024-03-01 15:17:44 -0300

    Update escola.js
    Adição do filtro e a variável com o array de notas dos estudantes.

--- escola.js
+++ escola.js
@@ -1,15 +1,27 @@
 //Exercício do módulo 25 EBAC
 
-const nomes = ['Renata', 'Roberta', 'Priscila', 'Rita', 'Ronaldo', 'Peterson'];
+//const nomes = ['Renata', 'Roberta', 'Priscila', 'Rita', 'Ronaldo', 'Peterson'];
 
-const notas = [8, 7, 9, 5, 4, 3];
+//const notas = [8, 7, 9, 5, 4, 3];
 
-nomes.forEach(function(nomesDosAlunos, indice) {
-    notas.forEach(function(notasAlunas, indices){
-    
-        if (indice===indices & notasAlunas>=6) {
-            console.log(`#${indice} Aluna(o)s: ${nomesDosAlunos} | Notas ${notasAlunas}`);
+let estudantes = [
+    { nomes: 'Renata',  nota: 8 },
+    { nomes: 'Roberta', nota: 9 },
+    { nomes: 'Priscila',nota: 5 },
+    { nomes: 'Rita',    nota: 5 },
+    { nomes: 'Ronaldo', nota: 6 },
+    { nomes: 'Peterson',nota: 4 }
+  ];
+
+
+/*estudantes.filter(function(nomesDosAlunos, resultado) {
+      
+        if (estudantes.nota>=6){
+          console.log(`Aluna(o)s: ${nomesDosAlunos} | Notas ${resultado}`);
         }
-        
-    })
-})
+     } )*/
+
+let alunosAprovados = estudantes.filter(aluno => aluno.nota >= 6)
+
+
+console.log(alunosAprovados)
