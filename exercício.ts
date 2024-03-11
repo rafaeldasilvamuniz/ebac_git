c99037075f5b2698581a9018bff241e63f08c321
Rafael da Silva Muniz <rafaelmunizrsm@hotmail.com>
2024-03-10 21:11:22 -0300

    TypeScript

---
+++ exercício.ts
@@ -0,0 +1,15 @@
+function multiplicacao(b: number,a: number): number {
+    return b * a;
+}
+
+function saudacao(nome:string){
+    console.log("Olá  " + nome); 
+}
+
+
+multiplicacao(6,8);
+
+saudacao('Teste');
+
+
+
