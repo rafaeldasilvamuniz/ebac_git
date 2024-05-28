8e8ba06ac5f0c7abdc5690cc5ad8f8ba6cc8b411
rafaeldasilvamuniz <86721430+rafaeldasilvamuniz@users.noreply.github.com>
2024-05-28 16:51:14 -0300

    Update App.vue - Alteração da Calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,166 +1,95 @@
+<script setup>
+//botão limpar
+  function Limpar(id1, id2){
+		document.getElementById(id1).value = "";
+		document.getElementById(id2).value = "";
+	}	
+
+  function Calcular(id1, id2){
+		//captura o valor do select(dropdown) selecionado
+		var operador = document.getElementById("operadores").options[document.getElementById("operadores").selectedIndex].value;
+		
+		var num1 = document.getElementById(id1).value;
+		var num2 = document.getElementById(id2).value;
+		var resultado = new Number();
+		//objeto com as funçoes dos algoritimos que retorna o resultado;
+		var calcular = {
+			fatorial: function(){
+				var strRes = 1;
+				for(i = num1; i > 1; i--){						
+					strRes *= i;
+				}
+				return num1+"! = "+strRes;
+			},
+	
+    }
+
+    switch(operador)
+		{
+			case "+":
+			case "-":
+			case "/":
+			case "*":
+				var StrExpressao = num1 + operador + num2;
+				//método eval realiza uma operaçao de strings e retorna do tipo number
+				resultado = eval(StrExpressao); break;					
+			/*case "raiz":
+				resultado = "&radic;"+num1 + " = " + Math.sqrt(num1); break;					
+			case "potencia":
+				resultado = num1+"<sup>"+num2 +"</sup> = "+ Math.pow(num1, num2); break;					
+			case "fatorial":
+				resultado = calcular['fatorial'](); break;					
+			case "fibonacci":
+				resultado = calcular['fibonacci'](); break;					
+			case "porcento":
+				resultado = calcular['porcentagem'](); break;
+			case "media":
+				resultado = calcular['media'](); break;
+			case "calc":
+				resultado = eval(num1); break;*/
+			default:
+				break;				
+			
+		}	
+
+    document.getElementById("saida").innerHTML = "Resultado: "+ 
+            
+    resultado;
+		
+
+  }
+
+</script>
+
 <template>
-  <div>
-   <header>
-    <h1>Calculadora</h1>
-   </header>
-   <main>
-    <label>Número 1 : </label>
-    
-   <div>
-    
-    <div v-if="calcularSoma">
-     <input
-      v-model="estado.input1"
-      @input="calcularSoma"
-      type="number"
-      class="form-control no-spinner"
-      placeholder="Adicionar Número"
-     />
-    </div>
 
-    <div v-else-if="calcularMultiplicacao">
-      <input v-model="estado.input1"
-       @input="calcularMultiplicacao" 
-       type="number"
-       class="form-control no-spinner"
-       placeholder="Adicionar Número"
-       >
-    </div>
-
-    <div v-else-if="calcularDiminuicao">
-      <input v-model="estado.input1"
-       @input="calcularDiminuicao" 
-       type="number"
-       class="form-control no-spinner"
-       placeholder="Adicionar Número"
-       >
-    </div>
-    
-    <div v-else-if="calcularDivisao">
-      <input v-model="estado.input1"
-       @input="calcularDivisao" 
-       type="number"
-       class="form-control no-spinner"
-       placeholder="Adicionar Número"
-       >
-    </div>
-
-  </div>
-
-    <br /><br />
-
-    <label>Número 2 : </label>
-
-  <div>
-    
-    <div v-if="calcularSoma">
-    <input
-     v-model="estado.input2"
-     @input="calcularSoma"
-     type="number"
-     class="form-control no-spinner"
-     placeholder="Adicionar Número"
-    />
-    </div>
-    
-    <div v-else-if="calcularMultiplicacao">
-    <input
-     v-model="estado.input2"
-     @input="calcularMultiplicacao"
-     type="number"
-     class="form-control no-spinner"
-     placeholder="Adicionar Número"
-    />
-    </div>
-
-    <div v-else-if="calcularDiminuicao">
-    <input
-     v-model="estado.input2"
-     @input="calcularDiminuicao"
-     type="number"
-     class="form-control no-spinner"
-     placeholder="Adicionar Número"
-    />
-    </div>
-
-     <div v-else-if="calcularDivisao">
-    <input
-     v-model="estado.input2"
-     @input="calcularDivisao"
-     type="number"
-     class="form-control no-spinner"
-     placeholder="Adicionar Número"
-    />
-    </div>
-
-  </div>
-
-    <br /><br />
-    
-    <label>Soma : </label>
-    <span>{{ estado.resultadoSoma }}</span><br><br>
-    
-
-    <label>Multiplicação : </label>
-      <span>{{ estado.resultadoMult }}</span><br><br>
-    
-
-    <label>Subtração : </label>
-    <span>{{ estado.resultadoDimin }}</span><br><br>
-
-
-    <label>Divisão : </label>
-      <span>{{ estado.resultadoDiv }}</span><br><br>
-    
+    <fieldset>
+    <body bgcolor="beige">
+		<h1>Calculadora</h1>
+		<label>Valor 1</label><br>
+		<input type="text" id="txtValor1"><br><br>
+		<label>Valor 2</label><br>
+		<input type="text" id="txtValor2" size="20"><br><br>	
+			<select id="operadores" onchange="ControleDeSelecao();">
+				<optgroup label="Basico">
+					<option value="+">	+(somar)</option>
+					<option value="-">	-(subtrair)</option>
+					<option value="*">	*(multiplicar)</option>
+					<option value="/">	/(dividir)</option>
+				</optgroup>
+				
+			</select>
+			
+		<input type="button" onclick="Calcular('txtValor1', 'txtValor2')" value="Calcular">
+		<input type="button" onclick="Limpar('txtValor1', 'txtValor2')" value="Limpar">
+		<h3 id="saida">Resultado:<span>{{ Calcular.resultado}}</span></h3>
+		<hr>		
+	</body>
+  </fieldset>
   
-  </main>
-  </div>
- </template>
- 
- <script setup>
- import { reactive } from "vue";
- 
- const estado = reactive({
-  input1: 0,
-  input2: 0,
-  resultadoSoma: 0,
-  resultadoMult: 0,
-  resultadoDimin: 0,
-  resultadoDiv:0
- });
- 
- const calcularSoma = () => {
-  estado.resultadoSoma = estado.input1 + estado.input2;
- };
+  
+</template>
 
- const calcularMultiplicacao = () => {
-  estado.resultadoMult = estado.input1 * estado.input2;
- };
+<style scoped>
 
- const calcularDiminuicao = () => {
-  estado.resultadoDimin = estado.input1 - estado.input2;
- };
-
- const calcularDivisao = () => {
-  estado.resultadoDiv = estado.input1 / estado.input2;
- };
- 
- </script>
- 
- <style scoped>
- /* Estilos opcionais */
-   /*@input="calcularResultado"*/
-   /*@input="calcularResultado"*/
- header {
-  background-color: #f0f0f0;
-  padding: 10px;
-  text-align: center;
- }
- main {
-  padding: 20px;
- }
- input[type="number"] {
-  width: 100px;
- }
- </style>
- 
+</style>
