37519df243ad104f97ccd7fe2ba14771c3969509
rafaeldasilvamuniz <86721430+rafaeldasilvamuniz@users.noreply.github.com>
2024-06-21 15:00:43 -0300

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,33 +5,9 @@ import './App.css'
 
 function App(){
 
-const form = document.querySelector('#form');
-
-/*form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const inputPeso = e.target.querySelector('#peso');
-    const inputAltura = e.target.querySelector('#altura');
-    const peso = Number(inputPeso.value);
-    const altura = Number(inputAltura.value);
-
-    if (!peso) {
-        setResultado('Peso inválido', false);
-        return;
-    }
-
-    if (!altura) {
-        setResultado('Altura inválida', false);
-        return;
-    }
-
-    const imc = getImc(peso, altura);
-    const nivelImc = getNivelImc(imc);
-
-    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
-
-    setResultado(msg, true);
-});*/
-
+const [altura, setAltura] = useState('');
+const [peso, setAltura] = useState('');
+const [imc, setAltura] = useState('');    
 
 function getNivelImc(imc) {
     const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
@@ -44,9 +20,10 @@ function getNivelImc(imc) {
     if (imc < 18.5) return nivel[0];
 }
 
-function getImc(peso, altura) {
-    const imc = peso / altura ** 2;
-    return imc.toFixed(2);
+function calculaImc(peso, altura) {
+    const imc = peso / ((altura/100) ** 2);
+    setImc(imc.toFixed(2));
+   // return imc.toFixed(2);
 }
 
 function criaP(className) {
@@ -54,21 +31,6 @@ function criaP(className) {
     return p;
 }
 
-function setResultado(msg, isValid) {
-    const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = '';
-
-    const p = criaP();
-
-    if (isValid) {
-        p.classList.add('paragrafo-resultado')
-    } else {
-        p.classList.add('erro');
-    }
-
-    p.innerHTML = msg;
-    resultado.appendChild(p);
-}
 
   return (
     <>
@@ -85,19 +47,19 @@ function setResultado(msg, isValid) {
        <label><b>Peso</b></label>
       </div>
       <div>
-        <input type="text" id="peso" name="peso"></input>
+        <input type="text" id="peso" name="peso" onChange={(e) => setPeso(e.target.value)}></input>
       </div>
       <div className="card">
              <label><b>Altura</b></label>
             </div>
     <div>
       <div>
-        <input type="text" id="altura" name="altura"></input>
+        <input type="text" id="altura" name="altura" onChange={(e) => setAlura(e.target.value)}></input>
        </div>
-      <button onClick={}>
+      <button onClick={calculaImc}>
                Enviar
        </button>
-       <p>Resultado: {}</p>
+       <p>Resultado: {getNivelImc()}</p>
     
     </div>
      
