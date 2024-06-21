b7e9e84ce9d6bc8b9cd739153f03ce0290784df1
rafaeldasilvamuniz <86721430+rafaeldasilvamuniz@users.noreply.github.com>
2024-06-21 15:03:38 -0300

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -12,12 +12,13 @@ const [imc, setAltura] = useState('');
 function getNivelImc(imc) {
     const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
 
-    if (imc >= 39.9) return nivel[5];
-    if (imc >= 34.9) return nivel[4];
-    if (imc >= 29.9) return nivel[3];
-    if (imc >= 24.9) return nivel[2];
-    if (imc >= 18.5) return nivel[1];
-    if (imc < 18.5) return nivel[0];
+    if (!imc) return '';
+    else if (imc >= 39.9) return nivel[5];
+    else if (imc >= 34.9) return nivel[4];
+    else if (imc >= 29.9) return nivel[3];
+    else if (imc >= 24.9) return nivel[2];
+    else if (imc >= 18.5) return nivel[1];
+    else if (imc < 18.5) return nivel[0];
 }
 
 function calculaImc(peso, altura) {
